907340c90a18a5d60e93b8826f90cd2855543378
Sava Petrovic <petrovicsava3@gmail.com>
2023-09-27 08:26:08 +0200

    Code cleaned up

--- src/core/filters/exceptions/basic-exception.filter.ts
+++ src/core/filters/exceptions/basic-exception.filter.ts
@@ -15,6 +15,7 @@ export class BasicExceptionFilter extends BaseExceptionFilter {
     const response = context.getResponse<Response>();
     const message = exception.getResponse()["message"];
 
+
     response.status(exception.getResponse()["statusCode"]).json({
       message,
       statusCode: exception.getResponse()["statusCode"],
